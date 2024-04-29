b829f5b1ccca9db83a78681299dbe2022d2d6875
unknown <brahimi.zakaria.abdessamed@gmail.com>
2024-04-29 11:02:50 +0100

    Adding Meta tags for Link Preview

--- frontend/src/components/CallUsTopBar.jsx
+++ frontend/src/components/CallUsTopBar.jsx
@@ -7,7 +7,7 @@ const CallUsTopBar = () => {
     const themeColor = platformData?.data?.result[0]?.primary_color
     
   return (
-    <header className={`bg-[#${themeColor}]  w-full sticky z-50 text-white top-0 left-0 text-center py-3`}>أطلب الآن! والدفع عند الاستلام | هاتف: 0{platformData?.data?.result[0]?.phone}</header>
+    <header className={`bg-[#fb4c54]  w-full sticky z-50 text-white top-0 left-0 text-center py-3`}>أطلب الآن! والدفع عند الاستلام | هاتف: 0{platformData?.data?.result[0]?.phone}</header>
   )
 }
 
